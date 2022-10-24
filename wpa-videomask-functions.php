644c5d75465505e2f99256ec14613d13d52efe1f
Metin Şaylan <metinsaylan@gmail.com>
2022-10-24 15:36:19 +0300

    add support for amp header

--- wpa-videomask-functions.php
+++ wpa-videomask-functions.php
@@ -21,8 +21,8 @@ function wpa_videomask_get_video_id_from_youtube_url(
 }
 
 
-add_filter( 
-'is_protected_meta', 
+add_filter(
+'is_protected_meta',
 function( $protected, $meta_key, $meta_type )
 {
 
@@ -33,3 +33,43 @@ function( $protected, $meta_key, $meta_type )
   return $protected;
 
 }, 10, 3 );
+
+
+
+function wpa_videomask_amp_content_has_iframe(){
+  global $wp_query;
+  if( !is_singular() ){ return false; }
+
+  the_post();
+  $content = apply_filters( 'the_content', get_the_content() );
+  rewind_posts();
+
+  return strpos( $content, '<amp-iframe' ) !== false;
+}
+
+function wpa_videomask_amp_content_has_youtube(){
+  global $wp_query;
+  if( !is_singular() ){ return false; }
+
+  the_post();
+  $content = apply_filters( 'the_content', get_the_content() );
+  rewind_posts();
+
+  return strpos( $content, '<amp-youtube' ) !== false;
+}
+
+
+// amp head support for wpxt
+add_action( 'amp_head', 'wpa_videomask_add_youtube_component' );
+function wpa_videomask_add_youtube_component(){
+
+  if( !is_singular() ) return;
+
+  if( wpa_videomask_amp_content_has_youtube() ){
+    echo '<script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>';
+  }
+  
+  if( wpa_videomask_amp_content_has_iframe() ){
+    echo '<script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>';
+  }
+}
